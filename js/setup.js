4e66ab862e7982f87fb57ef99598256d28c82d3d
Maksim Sianko <senkomaksim@gmail.com>
2018-12-19 12:42:44 -0800

    7th update

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,10 @@
 'use strict';
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 // Opening setup menu
 var userSetting = document.querySelector('.setup');
-userSetting.classList.remove('hidden');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
 // Finding template with data
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -44,6 +47,14 @@ var eyesColor = [
   'green'
 ];
 
+var fireballColor = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848' 
+];
+
 // Function for acquiring random element from an array
 var getRandomElement = function (arr) {
   var randomElement = Math.floor(Math.random() * arr.length);
@@ -70,7 +81,7 @@ var createWizard = function (wizardsAmount) {
 
 var cloneWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
+  
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coat;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyes;
@@ -87,3 +98,94 @@ var getWizardFragment = function (fragmentWizards) {
 
 similarListElement.appendChild(getWizardFragment(createWizard(4)));
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+// Event listeners
+var setupUsername = document.querySelector('.setup-user-name');
+var changeAppearance = document.querySelector('.setup-player');
+
+var inputCoat = changeAppearance.querySelector('input[name= "coat-color"]');
+var inputEyes = changeAppearance.querySelector('input[name= "eyes-color"]');
+var inputFireball = changeAppearance.querySelector('input[name= "fireball-color"]');
+
+var fireball = changeAppearance.querySelector('.setup-fireball-wrap');
+var wizardCoat = changeAppearance.querySelector('.wizard-coat');
+var wizardEyes = changeAppearance.querySelector('.wizard-eyes');
+
+// Changes fireball color on click
+var changeFireball = function () {
+ var newFireball = fireballColor[getRandomElement(fireballColor)];
+ fireball.style.background = newFireball;
+ inputFireball.value = newFireball;
+};
+
+fireball.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  changeFireball();
+});
+
+// Changes wizard's appearance on click
+var changeColor = function (arr, el,input) {
+  var newColor = coatColor[getRandomElement(coatColor)];
+  el.style.fill = newColor;
+  input.value = newColor;
+};
+
+wizardEyes.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  changeColor(eyesColor, wizardEyes, inputEyes);
+});
+
+wizardCoat.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  changeColor(coatColor, wizardCoat, inputCoat);
+});
+
+// Open/close events
+var closePopupEsc = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  userSetting.classList.remove('hidden');
+  document.addEventListener('keydown', closePopupEsc);
+}
+
+var closePopup = function () {
+  userSetting.classList.add('hidden');
+  document.addEventListener('keydown', closePopupEsc);
+}
+
+setupOpen.addEventListener('click', function() {
+   openPopup();
+})
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+})
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+})
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+})
+
+// Form's validation
+setupUsername.addEventListener('invalid', function () {
+  if (setupUsername.validity.tooShort) {
+    setupUsername.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (setupUsername.validity.tooLong) {
+    setupUsername.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (setupUsername.validity.valueMissing) {
+    setupUsername.setCustomValidity('Обязательное поле');
+  } else {
+    setupUsername.setCustomValidity('')
+  }
+})
