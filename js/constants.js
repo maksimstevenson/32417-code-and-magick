29465450c144077e838c149199039805fdec319c
Maksim Sianko <senkomaksim@gmail.com>
2019-01-14 20:32:38 -0800

    dialog

---
+++ js/constants.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+  window.constants = {
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
+    CLOUD_X: 100,
+    CLOUD_Y: 10,
+    GAP: 10,
+    CLOUD_WIDTH: 270,
+    CLOUD_HEIGHT: 420,
+    TEXT_X: 140,
+    TEXT_Y: 40,
+    HISTOGRAM_HEIGHT: 150,
+    COLUMN_WIDTH: 40,
+    COLUMN_GAP: 50,
+    FONT_GAP: 16,
+  };
+})();
+
--- js/setup.js
+++ js/setup.js
@@ -182,3 +182,123 @@ usernameInput.addEventListener('invalid', function () {
     usernameInput.setCustomValidity('');
   }
 });
+
+// Drag-n-drop
+// var setupDialogElement = document.querySelector('.setup');
+// var dialogHandler = setupDialogElement.querySelector('.upload');
+
+
+// dialogHandler.addEventListener('mousedown', function (evt) {
+//   evt.preventDefault();
+
+//   var startCoords = {
+//     x: evt.clientX,
+//     y: evt.clientY
+//   };
+
+//   var dragged = false;
+
+//   var onMouseMove = function (moveEvt) {
+//     moveEvt.preventDefault();
+//     dragged = true;
+
+//     var shift = {
+//       x: startCoords.x - moveEvt.clientX,
+//       y: startCoords.y - moveEvt.clientY
+//     };
+
+//     startCoords = {
+//       x: moveEvt.clientX,
+//       y: moveEvt.clientY
+//     };
+
+//     setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+//     setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
+//   };
+
+//   var onMouseUp = function (upEvt) {
+//     upEvt.preventDefault();
+
+//     document.removeEventListener('mousemove', onMouseMove);
+//     document.removeEventListener('mouseup', onMouseUp);
+
+//     if (dragged) {
+//       var onClickPreventDefault = function (deleteEvt) {
+//         deleteEvt.preventDefault();
+//         dialogHandler.removeEventListener('click', onClickPreventDefault);
+//       };
+//       dialogHandler.addEventListener('click', onClickPreventDefault);
+//     }
+//     var setupOpenElement = document.querySelector('.setup-open');
+//     setupOpenElement.addEventListener('click', function () {
+//       setupDialogElement.style.top = 80 + 'px';
+//       setupDialogElement.style.left = 50 + '%';
+//     });
+//   };
+
+//   document.addEventListener('mousemove', onMouseMove);
+//   document.addEventListener('mouseup', onMouseUp);
+// });
+
+// // Drag artifacts back and forth
+
+// var shopElement = document.querySelector('.setup-artifacts-shop');
+// var draggedItem = null;
+
+// shopElement.addEventListener('dragstart', function (evt) {
+//   if (evt.target.tagName.toLowerCase() === 'img') {
+//     draggedItem = evt.target;
+//     evt.dataTransfer.setData('text/plain', evt.target.alt);
+//   }
+// });
+// shopElement.addEventListener('dragover', function (evt) {
+//   evt.preventDefault();
+//   return false;
+// });
+
+// shopElement.addEventListener('drop', function (evt) {
+//   evt.target.style.backgroundColor = '';
+//   evt.target.appendChild(draggedItem);
+// });
+
+
+// shopElement.addEventListener('dragenter', function (evt) {
+//   evt.target.style.backgroundColor = 'yelow';
+//   evt.preventDefault();
+// });
+
+// shopElement.addEventListener('dragleave', function (evt) {
+//   evt.target.style.backgroundColor = '';
+//   evt.preventDefault();
+// });
+
+// var artifactsElement = document.querySelector('.setup-artifacts');
+
+// artifactsElement.addEventListener('dragover', function (evt) {
+//   evt.preventDefault();
+//   return false;
+// });
+
+// artifactsElement.addEventListener('drop', function (evt) {
+//   evt.target.style.backgroundColor = '';
+//   evt.target.appendChild(draggedItem);
+// });
+
+
+// artifactsElement.addEventListener('dragenter', function (evt) {
+//   evt.target.style.backgroundColor = 'yellow';
+//   evt.preventDefault();
+// });
+
+// artifactsElement.addEventListener('dragleave', function (evt) {
+//   evt.target.style.backgroundColor = '';
+//   evt.preventDefault();
+// });
+
+// artifactsElement.addEventListener('dragstart', function (evt) {
+//   if (evt.target.tagName.toLowerCase() === 'img') {
+//     draggedItem = evt.target;
+//     evt.dataTransfer.setData('text/plain', evt.target.alt);
+//   }
+// });
