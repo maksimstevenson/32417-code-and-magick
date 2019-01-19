1c14463265fc9d164b3ef865bf66cf95c15c9e66
Maksim Sianko <senkomaksim@gmail.com>
2019-01-18 23:14:56 -0800

    modulation

--- js/stat.js
+++ js/stat.js
@@ -1,58 +1,27 @@
 'use strict';
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10;
-var CLOUD_WIDTH = 270;
-var CLOUD_HEIGHT = 420;
-var TEXT_X = 140;
-var TEXT_Y = 40;
-var HISTOGRAM_HEIGHT = 150;
-var COLUMN_WIDTH = 40;
-var COLUMN_GAP = 50;
-var FONT_GAP = 16;
-var renderCloud = function (ctx, x, y, height, width, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
-};
 
-var renderText = function (ctx, str, x, y) {
-  ctx.fillStyle = '#17191b';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseLine = 'hanging';
-  ctx.fillText(str, x, y);
-};
-
-var getMaxElement = function (arr) {
-  var maxTime = arr[0];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxTime) {
-      maxTime = arr[i];
-    }
-  }
-  return maxTime;
-};
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0,0,0,0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
-  renderText(ctx, 'Ура вы победили!', TEXT_X, TEXT_Y);
-  renderText(ctx, 'Список результатов:', TEXT_X, TEXT_Y + GAP * 2);
-  var maxTime = getMaxElement(times);
+  window.utilities.renderCloud(ctx, window.constants.CLOUD_X + window.constants.GAP, window.constants.CLOUD_Y + window.constants.GAP, window.constants.CLOUD_WIDTH, window.constants.CLOUD_HEIGHT, 'rgba(0,0,0,0.7)');
+  window.utilities.renderCloud(ctx, window.constants.CLOUD_X, window.constants.CLOUD_Y, window.constants.CLOUD_WIDTH, window.constants.CLOUD_HEIGHT, '#fff');
+  window.utilities.renderText(ctx, 'Ура вы победили!', window.constants.TEXT_X, window.constants.TEXT_Y);
+  window.utilities.renderText(ctx, 'Список результатов:', window.constants.TEXT_X, window.constants.TEXT_Y + window.constants.GAP * 2);
+  var maxTime = window.utilities.getMaxElement(times);
   // message
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#121314';
   // Drawing histogram
   for (var i = 0; i < names.length; i++) {
-    var COL_HEIGHT = HISTOGRAM_HEIGHT * times[i] / maxTime;
+    var COL_HEIGHT = window.constants.HISTOGRAM_HEIGHT * times[i] / maxTime;
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random()) + ')';
     }
-    ctx.fillRect(CLOUD_X + COLUMN_GAP * (i + 1) + COLUMN_WIDTH * i, CLOUD_HEIGHT - COL_HEIGHT - COLUMN_GAP - FONT_GAP - 100, COLUMN_WIDTH, COL_HEIGHT);
+    ctx.fillRect(window.constants.CLOUD_X + window.constants.COLUMN_GAP * (i + 1) + window.constants.COLUMN_WIDTH * i, window.constants.CLOUD_HEIGHT - COL_HEIGHT - window.constants.COLUMN_GAP - window.constants.FONT_GAP - 100, window.constants.COLUMN_WIDTH, COL_HEIGHT);
     ctx.fillStyle = '#000';
     ctx.textBaseLine = 'hanging';
-    ctx.fillText(Math.floor(Math.random() + times[i]), CLOUD_X + COLUMN_GAP * (i + 1) + COLUMN_WIDTH * i, CLOUD_HEIGHT - COLUMN_GAP * 2 - FONT_GAP - COL_HEIGHT - 70);
-    ctx.fillText(names[i], CLOUD_X + COLUMN_GAP * (i + 1) + COLUMN_WIDTH * i, CLOUD_HEIGHT - HISTOGRAM_HEIGHT);
+    ctx.fillText(Math.floor(Math.random() + times[i]), window.constants.CLOUD_X + window.constants.COLUMN_GAP * (i + 1) + window.constants.COLUMN_WIDTH * i, window.constants.CLOUD_HEIGHT - window.constants.COLUMN_GAP * 2 - window.constants.FONT_GAP - COL_HEIGHT - 70);
+    ctx.fillText(names[i], window.constants.CLOUD_X + window.constants.COLUMN_GAP * (i + 1) + window.constants.COLUMN_WIDTH * i, window.constants.CLOUD_HEIGHT - window.constants.HISTOGRAM_HEIGHT);
   }
 };
---
+++ js/utilities.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  window.utilities = {
+
+    renderCloud: function (ctx, x, y, height, width, color) {
+      ctx.fillStyle = color;
+      ctx.fillRect(x, y, width, height);
+    },
+    renderText: function (ctx, str, x, y) {
+      ctx.fillStyle = '#17191b';
+      ctx.font = '16px PT Mono';
+      ctx.textBaseLine = 'hanging';
+      ctx.fillText(str, x, y);
+    },
+    getMaxElement: function (arr) {
+      var maxTime = arr[0];
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxTime) {
+          maxTime = arr[i];
+        }
+      }
+      return maxTime;
+    },
+  };
+})();
